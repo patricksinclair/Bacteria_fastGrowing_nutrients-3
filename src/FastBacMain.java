5e6f890b8d165a5e8b048326f1044f43270d7caa
Patrick Sinclair <patricksinclair@hotmail.co.uk>
2018-02-12 15:48:29 +0000

    first commit

---
+++ src/FastBacMain.java
@@ -0,0 +1,8 @@
+public class FastBacMain {
+    //this is a version of the fast growing bacteria with nutrients simulations, which include the more realistic
+    //growth curves
+    public static void main(String[] args){
+        double specific_alpha = Math.log(11.5)/500.;
+        BioSystem.spatialAndNutrientDistributions(0.02);
+    }
+}
